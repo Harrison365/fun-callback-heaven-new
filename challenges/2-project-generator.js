a347d006a994ebfd95222ced69e55a7f98ffcb75
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-10-26 12:49:38 +0100

    2-project-generator partially done

--- challenges/2-project-generator.js
+++ challenges/2-project-generator.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
 
-function projectGenerator(projectName, cb) {}
+function projectGenerator(projectName, cb) {
+  fs.mkdir(`./${projectName}`, err => {
+    if (err) {
+      cb(err);
+    } else {
+      fs.writeFile(`./${projectName}/index.js`, 'obligatory string', err => {
+        if (err) {
+          cb(err);
+        } else {
+          //do next thing
+        }
+      });
+    }
+  });
+}
+
+//no need for a second arguement in the mkdir or writeFile callbacks as they wont spit anything out that we can use
 
 module.exports = projectGenerator;
